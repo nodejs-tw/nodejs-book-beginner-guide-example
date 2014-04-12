5721c1e922886a99872b25e9ff9627c6cebc1a41
Po-Ying Chen <poying.me@gmail.com>
2014-04-12 11:41:33 +0800

    middlewares

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var routers = require('./routers');
 
 var app = module.exports = express();
+
+app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat', key: 'sid', cookie: { secure: true }}));
+app.use(bodyParser());
 
 routers(app);
 
